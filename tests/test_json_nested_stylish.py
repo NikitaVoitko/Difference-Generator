a20a28159f911f5a20785f48228df8fa87466934
Nikita <voitko1994@gmail.com>
2024-05-12 12:57:36 +0200

    First final step commit

---
+++ tests/test_json_nested_stylish.py
@@ -0,0 +1,17 @@
+import os
+from gendiff.engine import generate_diff
+
+
+def test_generate_diff_json():
+    path1 = os.path.join(os.path.dirname(__file__), 'fixtures/input_files/',
+                         'file3.json')
+    path2 = os.path.join(os.path.dirname(__file__), 'fixtures/input_files/',
+                         'file4.json')
+
+    expected_path = os.path.join(os.path.dirname(__file__),
+                                 'fixtures/result_files/stylish/',
+                                 'result_json_nested.txt')
+    with open(expected_path, 'r') as file:
+        expected = file.read()
+
+    assert generate_diff(path1, path2) == expected
---
+++ tests/test_yml_nested_stylish.py
@@ -0,0 +1,17 @@
+import os
+from gendiff.engine import generate_diff
+
+
+def test_generate_diff_json():
+    path1 = os.path.join(os.path.dirname(__file__), 'fixtures/input_files/',
+                         'file3.yml')
+    path2 = os.path.join(os.path.dirname(__file__), 'fixtures/input_files/',
+                         'file4.yml')
+
+    expected_path = os.path.join(os.path.dirname(__file__),
+                                 'fixtures/result_files/stylish/',
+                                 'result_yml_nested.txt')
+    with open(expected_path, 'r') as file:
+        expected = file.read()
+
+    assert generate_diff(path1, path2) == expected
